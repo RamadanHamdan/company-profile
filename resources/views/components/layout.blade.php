3271534df024e038116181deec4fbf2678e75b61
Ramadan <ramadanhamdan98@gmail.com>
2025-05-08 21:27:41 +0700

    Mengubah app

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html class="class="h-full bg-gray-100 lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pages</title>
+</head>
+<body class="h=full">
+
+<div class="min-h-full">
+  <x-navbar></x-navbar>
+
+  {{-- <x-header>{{ $title }}</x-header> --}}
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <!-- Your content -->
+      {{ $slot }}
+    </div>
+  </main>
+</div>
+{{-- <x-footer></x-footer> --}}
+</body>
+</html>
